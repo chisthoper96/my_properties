a05b3eec7ac8bea1611638f8bea76040473e8120
chisthoper <chisthoper96@gmail.com>
2023-04-07 11:11:46 -0400

    validate images

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,10 +5,22 @@ class Property < ApplicationRecord
   # validations
   validates :price, numericality: { greater_than: 0 }
   validates :operation, :property_type, :currency_type, :commune, presence: true
+  validate :validate_images
   
   # enum
   enum operation: [:VENTA, :ARRIENDO]
   enum property_type: [:CASA, :DEPTO, :OFICINA, :TERRENO]
   enum currency_type: [:CLP, :UF]
 
+  # activestorage
+  has_many_attached :images
+
+  private 
+
+  def validate_images
+    unless images.attached? && images.length >= 3
+      errors.add(:images, "Debe ingresar un min de 3 imagenes")
+    end
+  end
+  
 end
