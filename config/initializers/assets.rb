8406cd5c94b51afed1c12dd5258c5b39132e9eeb
chisthoper <chisthoper96@gmail.com>
2023-04-07 11:13:03 -0400

    export xlsx

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( jquery.min.js jquery_ujs.js jquery-ui.min.js )
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
