43c286a140dbda34aaf7f40676f5eea0c2a8d2d6
chisthoper <chisthoper96@gmail.com>
2023-04-07 11:11:57 -0400

    routes property

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :properties do
+    collection do
+      get :export_xlsx
+      get :export_pdf
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
