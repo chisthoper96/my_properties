88045a88256f0e34e2bb3c234f9a07fd88d7c2f0
chisthoper <chisthoper96@gmail.com>
2023-04-07 11:11:29 -0400

    controller properties

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,70 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: [:update, :destroy]
+
+  def index
+    @properties = current_user.properties
+  end
+
+  def show
+    @property = current_user.properties.where(id: params[:id]).last
+    if @property.nil?
+      flash[:alert] = "No estas autorizado para ver la propiedad"
+      redirect_to properties_path
+    end
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def edit
+    @property = current_user.properties.where(id: params[:id]).last
+    if @property.nil?
+      flash[:alert] = "No estas autorizado para ver la propiedad"
+      redirect_to properties_path
+    end
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      redirect_to @property, notice: 'Propiedad Creada correctamente!'
+    else
+      redirect_to new_property_path,  alert: 'No se pudo crear la propiedad' 
+    end
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to @property, notice: 'Property was successfully updated.'
+    else
+      redirect_to edit_property_path(@property),  alert: 'No se pudo actualizar la propiedad' 
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_url, notice: 'Se ha eliminado correctamente'
+  end
+
+  def export_xlsx
+    @properties = current_user.properties
+    respond_to do |format|
+      format.xlsx { render xlsx: 'export_xlsx', filename: "reporte_propiedades.xlsx"}
+    end
+  end
+
+  def export_pdf
+  end
+
+  private
+
+  def set_property 
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:operation, :property_type, :price, :currency_type, :commune, :address, :area, :bedrooms, :bathrooms, :user_id, images: [])
+  end
+end
